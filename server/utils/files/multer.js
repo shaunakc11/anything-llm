aecdc80e624202c73b47787d0c362b712eb4b391
Sukomal07 <hypersukomal@gmail.com>
2024-10-04 13:12:59 +0530

    Reverted content of file

--- server/utils/files/multer.js
+++ server/utils/files/multer.js
@@ -2,23 +2,8 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 const { v4 } = require("uuid");
-
-// Handle File uploads for auto-uploading.
-const fileUploadStorage = multer.diskStorage({
-  destination: function (_, __, cb) {
-    const uploadOutput =
-      process.env.NODE_ENV === "development"
-        ? path.resolve(__dirname, `../../../collector/hotdir`)
-        : path.resolve(process.env.STORAGE_DIR, `../../collector/hotdir`);
-    cb(null, uploadOutput);
-  },
-  filename: function (_, file, cb) {
-    file.originalname = Buffer.from(file.originalname, "latin1").toString(
-      "utf8"
-    );
-    cb(null, file.originalname);
-  },
-});
+const { S3Service } = require("../aws");
+const s3Service = new S3Service();
 
 // Asset storage for logos
 const assetUploadStorage = multer.diskStorage({
@@ -57,19 +42,65 @@ const pfpUploadStorage = multer.diskStorage({
 
 // Handle Generic file upload as documents
 function handleFileUpload(request, response, next) {
-  const upload = multer({ storage: fileUploadStorage }).single("file");
-  upload(request, response, function (err) {
+  const bucketName = process.env.S3_BUCKET_NAME;
+  const upload = multer({ storage: multer.memoryStorage() }).single("file");
+
+  upload(request, response, async function (err) {
     if (err) {
-      response
-        .status(500)
-        .json({
-          success: false,
-          error: `Invalid file upload. ${err.message}`,
-        })
-        .end();
-      return;
+      // Handle Multer errors
+      return response.status(500).json({
+        success: false,
+        error: `Invalid file upload. ${err.message}`,
+      });
     }
-    next();
+
+    if (!request.file) {
+      // Handle case where no file was uploaded
+      return response.status(400).json({
+        success: false,
+        error: "No file uploaded.",
+      });
+    }
+
+    try {
+      const { originalname, buffer } = request.file;
+      console.log(`-- Working ${originalname} --`);
+
+      const uuid = v4();
+      const uniqueFilename = `${uuid}-${originalname}`;
+
+      // Upload the file buffer to S3
+      //TODO: set the ContentType to request.file.mimetype
+      const params = {
+        Bucket: bucketName,
+        Key: uniqueFilename,
+        Body: buffer,
+        ContentType: request.file.mimetype, // Set the ContentType
+      };
+
+      const fileUploadUrl = await s3Service.uploadFileToS3(
+        undefined,
+        undefined,
+        undefined,
+        params
+      );
+
+      // Attach variables to the request object
+      request.uploadedFile = {
+        url: fileUploadUrl,
+        filename: uniqueFilename,
+        uuid: uuid,
+        originalname: originalname,
+      };
+
+      next();
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      return response.status(500).json({
+        success: false,
+        error: "Error processing file upload.",
+      });
+    }
   });
 }
 
